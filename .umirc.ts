c28bee6a7773e9e839fbb6cd726968367821f8bf
Matthew <Matthrews@outlook.com>
2021-05-29 18:07:21 +0800

    chore: make a logo for x-design

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'x-design',
-  favicon:
-    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon: 'https://cdn.jsdelivr.net/gh/Matthrews/zm_cdn/images/x-design.png',
+  logo: 'https://cdn.jsdelivr.net/gh/Matthrews/zm_cdn/images/x-design.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
 });
